3d143e197d4b73c34ab20337c707d6923f4e4ad8
Phan Anh Khoa <88132636+KhoaPhan1504@users.noreply.github.com>
2023-04-21 22:40:05 +0700

    Add files via upload

---
+++ Tuan7/ConvertDecimalToBinary/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+namespace Khoa.IT
+{
+    class Program
+    {
+        public static int CHAR_55 = 55;
+        public static int CHAR_48 = 48;
+ 
+        static void Main(string[] args)
+        {
+            int n = 14;
+            Console.Write("So {0} trong he co so 2 = ", n);
+            convertNumber(n, 2);
+            Console.Write("\nSo {0} trong he co so 16 = ", n);
+            convertNumber(n, 16);
+ 
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+ 
+ 
+        /**
+         * chuyen doi so nguyen n sang he co so b
+         * 
+         * @author viettuts.vn
+         * @param n: so nguyen
+         * @param b: he co so
+         */
+        static int convertNumber(int n, int b)
+        {
+            if (n < 0 || b < 2 || b > 16)
+            {
+                Console.Write("He co so hoac gia tri chuyen doi khong hop le!");
+                return 0;
+            }
+            int i;
+            char[] arr = new char[20];
+            int count = 0;
+            int m;
+            int remainder = n;
+            while (remainder > 0)
+            {
+                if (b > 10)
+                {
+                    m = remainder % b;
+                    if (m >= 10)
+                    {
+                        arr[count] = (char)(m + CHAR_55);
+                        count++;
+                    }
+                    else
+                    {
+                        arr[count] = (char)(m + CHAR_48);
+                        count++;
+                    }
+                }
+                else
+                {
+                    arr[count] = (char)((remainder % b) + CHAR_48);
+                    count++;
+                }
+                remainder = remainder / b;
+            }
+            // hien thi he co so
+            for (i = count - 1; i >= 0; i--)
+            {
+                Console.Write("{0}", arr[i]);
+            }
+            return 1;
+        }
+    }
+}
---
+++ Tuan7/Euclid/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Khoa.IT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Nhap so nguyen duong a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so nguyen duong b = ");
+            b = int.Parse(Console.ReadLine());
+            // tinh USCLN cua a và b
+            Console.WriteLine("USCLN cua {0} va {1} la: {2}", a, b, USCLN(a, b));
+            // tinh BSCNN cua a và b
+            Console.WriteLine("USCLN cua {0} va {1} la: {2}", a, b, BSCNN(a, b));
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+
+        /**
+        * Tim uoc so chung lon nhat (USCLN)
+        */
+        static int USCLN(int a, int b)
+        {
+            if (b == 0) return a;
+            return USCLN(b, a % b);
+        }
+
+        /**
+         * Tim boi so chung nho nhat (BSCNN)
+         */
+        static int BSCNN(int a, int b)
+        {
+            return (a * b) / USCLN(a, b);
+        }
+    }
+}
